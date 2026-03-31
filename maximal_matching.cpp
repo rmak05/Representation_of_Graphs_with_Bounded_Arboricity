5e467bedca97da111359f5f500e83da3c118c365
Ravilisetty Makarandh <rmak1969@gmail.com>
2026-03-31 22:16:33 +0530

    Completed insert and build. Maximal Matching done.

--- maximal_matching.cpp
+++ maximal_matching.cpp
@@ -4,7 +4,12 @@
 graph_maximal_matching::list_node::list_node(const int _vertex, list_node *_next, list_node *_previous, list_node *_external) :
 vertex(_vertex), next(_next), previous(_previous), external(_external){}
 
-void graph_maximal_matching::insert_list_node(list_node*& _head, list_node* _node){
+graph_maximal_matching::linked_list::linked_list() :
+head(nullptr), size(0){}
+
+void graph_maximal_matching::insert_list_node(linked_list& _list, list_node* _node){
+    list_node *_head = _list.head;
+
     if(!_node) return;
 
     _node->next = _head;
@@ -12,9 +17,14 @@ void graph_maximal_matching::insert_list_node(list_node*& _head, list_node* _nod
     if(_head) _head->previous = _node;
 
     _head = _node;
+
+    _list.size++;
 }
 
-void graph_maximal_matching::erase_list_node(list_node*& _head, list_node* _node){
+// void graph_maximal_matching::erase_list_node(list_node*& _head, list_node* _node){
+void graph_maximal_matching::erase_list_node(linked_list& _list, list_node* _node){
+    list_node *_head = _list.head;
+
     if(!_head || !_node) return;
 
     if(_head == _node){
@@ -31,9 +41,14 @@ void graph_maximal_matching::erase_list_node(list_node*& _head, list_node* _node
 
         delete _node;
     }
+
+    _list.size--;
 }
 
-bool graph_maximal_matching::erase_vertex_from_out_list(list_node*& _head, int _vertex){
+// bool graph_maximal_matching::erase_vertex_from_out_list(list_node*& _head, int _vertex){
+bool graph_maximal_matching::erase_vertex_from_out_list(linked_list& _list, int _vertex){
+    list_node *_head = _list.head;
+
     if(!_head) return false;
 
     list_node *temp = _head;
@@ -47,7 +62,7 @@ bool graph_maximal_matching::erase_vertex_from_out_list(list_node*& _head, int _
                 erase_list_node(in_list_matched[temp->vertex], temp->external);
             }
             
-            erase_list_node(_head, temp);
+            erase_list_node(_list, temp);
 
             return true;
         }
@@ -59,7 +74,7 @@ bool graph_maximal_matching::erase_vertex_from_out_list(list_node*& _head, int _
 }
 
 void graph_maximal_matching::notify_out_neighbours(int _u, bool _matched){
-    list_node *list_itr = out_list[_u];
+    list_node *list_itr = out_list[_u].head;
     
     while(list_itr){
         int x = list_itr->vertex;
@@ -83,45 +98,76 @@ void graph_maximal_matching::notify_out_neighbours(int _u, bool _matched){
     }
 }
 
-// graph_maximal_matching::graph_maximal_matching(const int _num_vertices, const int _arboricity){
-//     graph_maximal_matching::build(_num_vertices, std::vector<std::pair<int, int>>(), _arboricity);
-// }
+graph_maximal_matching::graph_maximal_matching(){
+    build(0, std::vector<std::pair<int, int>>(), 0);
+}
 
-// void graph_maximal_matching::insert(const int _u, const int _v){
-//     int delta = 4 * arboricity;
+graph_maximal_matching::graph_maximal_matching(const int _num_vertices, const int _arboricity){
+    build(_num_vertices, std::vector<std::pair<int, int>>(), _arboricity);
+}
 
-//     /*
-//     if _u and _v are unmatched, match them immediately
-//     */
-    
-//     /*
-//     below lines update the list correctly but it is supposed to match as well if possible
-//     */
-//     out_list[_u].push_back(_v);
-//     if(matching[_u] == -1) partial_unmatched_list[_v].insert(_u);
-//     if(matching[_v] == -1) partial_unmatched_list[_u].insert(_v);
+graph_maximal_matching::graph_maximal_matching(const int _num_vertices, const std::vector<std::pair<int, int>>& _edges, const int _arboricity){
+    build(_num_vertices, _edges, _arboricity);
+}
 
-//     if(static_cast<int>(out_list[_u].size()) == delta + 1){
-//         std::stack<int> stk;
+void graph_maximal_matching::insert(const int _u, const int _v){
+    int delta = 4 * arboricity;
 
-//         stk.push(_u);
-//         while(!stk.empty()){
-//             int w = stk.top();
-//             stk.pop();
+    list_node* u_node = new list_node(_u, nullptr, nullptr, nullptr);
+    list_node* v_node = new list_node(_v, nullptr, nullptr, nullptr);
+    u_node->external = v_node;
+    v_node->external = u_node;
+    insert_list_node(out_list[_u], v_node);
+    if(matching[_u] != -1){
+        insert_list_node(in_list_matched[_v], u_node);
+    }
+    else{
+        if(matching[_v] == -1){
+            matching[_u] = _v;
+            matching[_v] = _u;
 
-//             for(auto& x : out_list[w]){
-//                 out_list[x].push_back(w);
-//                 if(matching[x] == -1) partial_unmatched_list[w].insert(x);
+            notify_out_neighbours(_u, true);
+            notify_out_neighbours(_v, true);
 
-//                 if(static_cast<int>(out_list[x].size()) == delta + 1){
-//                     stk.push(x);
-//                 }
-//             }
+            insert_list_node(in_list_matched[_v], u_node);
+        }
+        else{
+            insert_list_node(in_list_unmatched[_v], u_node);
+        }
+    }
 
-//             out_list[w].clear();
-//         }
-//     }
-// }
+    if(out_list[_u].size  == delta + 1){
+        std::stack<int> stk;
+
+        stk.push(_u);
+        while(!stk.empty()){
+            int w = stk.top();
+            stk.pop();
+
+            while(out_list[w].size > 0){
+                int x = out_list[w].head->vertex;
+
+                erase_vertex_from_out_list(out_list[w], x);
+
+                list_node* w_node = new list_node(w, nullptr, nullptr, nullptr);
+                list_node* x_node = new list_node(x, nullptr, nullptr, nullptr);
+                w_node->external = x_node;
+                x_node->external = w_node;
+                insert_list_node(out_list[x], w_node);
+                if(matching[x] != -1){
+                    insert_list_node(in_list_matched[w], x_node);
+                }
+                else{
+                    insert_list_node(in_list_unmatched[w], x_node);
+                }
+
+                if(out_list[x].size == delta + 1){
+                    stk.push(x);
+                }
+            }
+        }
+    }
+}
 
 void graph_maximal_matching::erase(int _u, int _v){
     if(erase_vertex_from_out_list(out_list[_u], _v)){
@@ -143,7 +189,7 @@ void graph_maximal_matching::erase(int _u, int _v){
     for(int w : {_u, _v}){
         if(matching[w] != -1) continue;
 
-        list_node *list_itr = out_list[w];
+        list_node *list_itr = out_list[w].head;
     
         while(list_itr){
             int x = list_itr->vertex;
@@ -163,15 +209,19 @@ void graph_maximal_matching::erase(int _u, int _v){
     }
 }
 
-// void graph_maximal_matching::build(const int _num_vertices, const std::vector<std::pair<int, int>>& _edges, const int _arboricity){
-//     sparse_graph::build(_num_vertices, _edges, _arboricity);
+void graph_maximal_matching::build(const int _num_vertices, const std::vector<std::pair<int, int>>& _edges, const int _arboricity){
+    arboricity = _arboricity;
 
-//     matching.clear();
-//     matching.resize(_num_vertices, -1);
+    matching.clear();
+    out_list.clear();
+    in_list_matched.clear();
+    in_list_unmatched.clear();
+    matching.resize(_num_vertices, -1);
+    out_list.resize(_num_vertices);
+    in_list_matched.resize(_num_vertices);
+    in_list_unmatched.resize(_num_vertices);
 
-//     out_list.clear();
-//     out_list.resize(_num_vertices);
-
-//     partial_unmatched_list.clear();
-//     partial_unmatched_list.resize(_num_vertices);
-// }
+    for(auto& edge :_edges){
+        insert(edge.first, edge.second);
+    }
+}
--- maximal_matching.hpp
+++ maximal_matching.hpp
@@ -15,18 +15,25 @@ private:
         list_node(const int _vertex = -1, list_node *_next = nullptr, list_node *_previous = nullptr, list_node *_external = nullptr);
     };
 
-    void insert_list_node(list_node*& _head, list_node* _node);
-    void erase_list_node(list_node*& _head, list_node* _node); /* _node must exist in _head */
-    bool erase_vertex_from_out_list(list_node*& _head, int _vertex); /* returns true if erased successfully */
+    struct linked_list{
+    public:
+        list_node* head;
+        int size;
+
+        linked_list();
+    };
+
+    void insert_list_node(linked_list& _list, list_node* _node);
+    void erase_list_node(linked_list& _list, list_node* _node); /* _node must exist in _head */
+    bool erase_vertex_from_out_list(linked_list& _list, int _vertex); /* returns true if erased successfully */
     void notify_out_neighbours(int _u, bool _matched);
 
 protected:
-    int                     arboricity;
-    std::vector<int>        matching; /* if v is matched to u, matching[v] = u, else matching[v] = -1 */
-    std::vector<list_node*> in_list_matched;
-    std::vector<list_node*> in_list_unmatched;
-    std::vector<list_node*> out_list;
-    // std::vector<std::set<int>>      partial_unmatched_list;
+    int                         arboricity;
+    std::vector<int>            matching; /* if v is matched to u, matching[v] = u, else matching[v] = -1 */
+    std::vector<linked_list>    in_list_matched;
+    std::vector<linked_list>    in_list_unmatched;
+    std::vector<linked_list>    out_list;
     
 public:
     graph_maximal_matching();
